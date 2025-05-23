393ebb771dbe81447ed142b891c6694dbcdf53a5
unknown <william.hernandez-d@labinfo.is.escuelaing.edu.co>
2025-05-23 15:24:04 -0500

    scaffolding

---
+++ src/main/java/eci/edu/cvds/ECISalud/EciSaludApplication.java
@@ -0,0 +1,13 @@
+package eci.edu.cvds.ECISalud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EciSaludApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EciSaludApplication.class, args);
+	}
+
+}
